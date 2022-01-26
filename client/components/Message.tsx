b53fdccbcc328cde56fbc6ee93140d4741a01398
salaadas <carbonfibersweeper@gmail.com>
2022-01-26 15:40:29 +0700

    message hover

--- client/components/Message.tsx
+++ client/components/Message.tsx
@@ -7,7 +7,7 @@ interface MessageProps {
 
 export const Message: React.FC<MessageProps> = ({ name, content }) => {
   return (
-    <div className="w-full flex flex-row items-center justify-between py-2 px-16 m-0">
+    <div className="w-full flex flex-row items-center justify-between py-2 px-16 m-0 hover:bg-custom-accent">
       <div className="flex flex-col items-center w-12 m-0 ml-2 mb-auto">
         <img
           src={`https://avatars.dicebear.com/api/open-peeps/seed.svg`}
@@ -16,7 +16,7 @@ export const Message: React.FC<MessageProps> = ({ name, content }) => {
         />
       </div>
 
-      <div className="w-11/12 flex flex-col justify-start hover:bg-yellow-300">
+      <div className="w-11/12 flex flex-col justify-start">
         <p className="text-left font-semibold text-custom-light mr-1 cursor-pointer">
           {name}
         </p>
